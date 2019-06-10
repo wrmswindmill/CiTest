81f5564706569a55b0f6d713083907f02242b2c1
wrm <wrmwindmill@gmail.com>
2019-06-10 18:37:16 +0800

    init

---
+++ CiTest/CiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CiTest
+//
+//  Created by WM on 2019/6/10.
+//  Copyright © 2019 WM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
